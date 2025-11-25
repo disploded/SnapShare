021626f1ba01070f3d675b3bfa398adcebed68fb
Alper <alperisbeast@gmail.com>
2025-11-26 10:53:00 +1300

    File input displays names

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,49 @@
+'use client'
 import Image from "next/image";
+import './globals.css';
+import React, { useState } from "react";
 
 export default function Home() {
+function fileChange(e: React.ChangeEvent<HTMLInputElement>) {
+  const fileText = document.getElementById('fileText');
+  if (!e.target.files) return;
+  if (!fileText) return;
+
+  fileText.textContent = '';
+
+  for (let step = 0; step < e.target.files.length; step++) {
+    const file = e.target.files[step]
+    fileText.textContent += `${file.name}\n`;
+  }
+}
+
+  function submitFiles() {
+    console.log("submit"); //get file data and put into a room. use code generation & password (optional)
+  }
+
+  function joinButton() {
+    console.log("joining");
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+      <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
+         <label id="fileText" className="whitespace-pre-wrap font-bold text-3xl border-4 rounded-2xl p-30 bg-gray-200" htmlFor="dragdrop">
+            <input multiple type="file" id="dragdrop" onChange={fileChange} className="hidden"></input>
+          Import files</label>
+          <div className="flex flex-col">
+
+            <button id="submitButton" 
+            className="border-4 py-5 px-10 my-3 bg-green-400 font-semibold text-2xl"
+            onClick={submitFiles}>Submit files</button>
+
+            <h1 className="font-medium text-2xl">or if you have a code</h1>
+
+            <button id="joinButton"
+            className="border-4 py-5 px-10 bg-blue-400 font-semibold text-2xl"
+            onClick={joinButton}>Join server</button>
+
+          </div>
       </main>
     </div>
   );
