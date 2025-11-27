1390d1ee9e9347fbfce4e8b3d78a8dd2a28ba07d
Alper <alperisbeast@gmail.com>
2025-11-27 18:30:07 +1300

    Render image previews with useState()

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import './globals.css';
 import React, { useState } from "react";
 
 export default function Home() {
+const [previewImages, setPreviewImages] = useState<string[]>([]);
+
 function fileChange(e: React.ChangeEvent<HTMLInputElement>) {
   const fileText = document.getElementById('fileText');
   if (!e.target.files) return;
@@ -11,9 +13,17 @@ function fileChange(e: React.ChangeEvent<HTMLInputElement>) {
 
   fileText.textContent = '';
 
-  for (let step = 0; step < e.target.files.length; step++) {
-    const file = e.target.files[step]
-    fileText.textContent += `${file.name}\n`;
+  for (let step = 0; step < e.target.files.length; step++) { //ADD function that prevents file overload
+    const file = e.target.files[step]                        //(when you try upload more files on second click
+    fileText.textContent += `${file.name}\n`;                //it deletes the previous files)
+
+    if (file.type.startsWith("image/")) { //if file is image: create a preview
+      console.log("recieved")
+
+      const url = URL.createObjectURL(file);
+      //create image element and insert with url as src parameter
+      setPreviewImages(prev => [...prev, url]);
+    }
   }
 }
 
@@ -28,19 +38,29 @@ function fileChange(e: React.ChangeEvent<HTMLInputElement>) {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
-         <label id="fileText" className="whitespace-pre-wrap font-bold text-3xl border-4 rounded-2xl p-30 bg-gray-200" htmlFor="dragdrop">
-            <input multiple type="file" id="dragdrop" onChange={fileChange} className="hidden"></input>
-          Import files</label>
+          <div className="border-cyan-800 flex flex-col justify-between items-center whitespace-pre-wrap font-bold text-3xl border-4 rounded-2xl min-h-82 min-w-1/2 bg-gray-200">
+          <label id="fileText" className="" htmlFor="dragdrop">
+          Import files
+          </label>
+          <input multiple type="file" id="dragdrop" onChange={fileChange} className="hidden"></input>
+            <div id="previewContainer" 
+              className="flex gap-6 py-8 px-2 bg-cyan-800 min-w-full text-3xl font-medium text-blue-100"
+            >
+            <h1>Preview:</h1>
+            {previewImages.map((item, i) => {return (<img src={item} key={i}></img>)})}
+
+            </div>
+          </div>
           <div className="flex flex-col">
 
             <button id="submitButton" 
-            className="border-4 py-5 px-10 my-3 bg-green-400 font-semibold text-2xl"
+            className="border-4 border-green-200 py-5 px-10 my-3 bg-green-400 font-semibold text-2xl"
             onClick={submitFiles}>Submit files</button>
 
             <h1 className="font-medium text-2xl">or if you have a code</h1>
 
             <button id="joinButton"
-            className="border-4 py-5 px-10 bg-blue-400 font-semibold text-2xl"
+            className="border-4 border-blue-200 py-5 px-10 bg-blue-400 font-semibold text-2xl"
             onClick={joinButton}>Join server</button>
 
           </div>
