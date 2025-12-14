d2dc2a8e3fe4ab5fe9decb6e4bc57f1d63128a3e
Alper <alperisbeast@gmail.com>
2025-12-14 15:26:27 +1300

    Pair serverio with client side code

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.prepare().then(() => {
   const io = new Server(httpServer);
 
   io.on("connection", (socket) => {
-    // ...
+    console.log(`User ${socket.id} has connected`)
   });
 
   httpServer
