9139cf8916ae56d205d8c66035d550769de7ae4e
Alper <alperisbeast@gmail.com>
2026-03-20 21:22:08 +1300

    Socket receives roomCount updates & displays visuals

--- app/rooms/[roomId]/page.tsx
+++ app/rooms/[roomId]/page.tsx
@@ -1,26 +1,40 @@
 'use client';
 import { useParams } from "next/navigation";
-import { socket } from '../../../socket.js';
 import FileInput from "@/app/components/FileInput.js";
+import { useEffect, useState } from "react";
+import { socket } from '../../../socket.js';
 
 export default function RoomPage() {
   const params = useParams<{ roomId: string }>();
+  const [roomCount, setRoomCount] = useState(0);
 
-  socket.on("connection", (socket) => {
-    socket.join(`${params.roomId}`)
-  })
+  useEffect(() => {
+    if (!params.roomId) return; // don't emit if id isn't ready
+    socket.emit("joinRoom", (params.roomId));
+
+    socket.on("roomUpdate", (newRoomCount) => {
+      setRoomCount(newRoomCount);
+    });
+
+    return () => {
+      socket.off("roomUpdate")
+    };
+  }, [params.roomId]);
 
 return (
   <div className="min-h-screen flex items-center justify-center bg-zinc-100 px-4">
     <div className="w-full max-w-3xl rounded-2xl border border-zinc-200 bg-white">
 
-      <div className="border-b border-zinc-200 dark:border-zinc-800 px-8 py-6">
-        <h1 className="text-2xl font-bold tracking-tight">
-          Room: <span className="text-emerald-500">{params.roomId}</span>
-        </h1>
-        <p className="text-sm text-zinc-500 mt-1">
-          Upload files to share in this room
-        </p>
+      <div className="flex justify-between border-b border-zinc-200 dark:border-zinc-800 px-8 py-6">
+        <div>
+          <h1 className="text-2xl font-bold tracking-tight">
+            Room: <span className="text-emerald-500">{params.roomId}</span>
+          </h1>
+          <p className="text-sm text-zinc-500 mt-1">
+            Upload files to share in this room
+          </p>
+        </div>
+        <p className="text-2xl font-bold">In Room: {roomCount}</p>
       </div>
 
       <div className="px-8 py-6">
